7cea4fa8069ec59d4f3e760e3a25a9a672d38a4e
Akhilesh-SDET <akhilesh.kumar95@outlook.com>
2026-05-29 14:30:39 +0530

    Arithmetic operator

---
+++ JavaScripting/3-Operator/1-ArithmeticOperator.js
@@ -0,0 +1,53 @@
+ //Arthmetic Opertor
+ //Addition operator And Concadition
+
+console.log(7+6);//13
+console.log("100"+10)//10010
+console.log("100"+typeof null);
+console.log(200+150+"200")
+console.log(true+100);
+console.log(2+false)
+console.log(3+null)
+console.log(3+undefined)//NaN
+
+//concadition
+console.log("Akhilesh"+"Kumar")//AkhileshKumar
+console.log("Akhilesh"+100)//Akhilesh100
+console.log("Akhilesh"+10)//Akhilesh10
+console.log("Akhilesh"+true)//Akhileshtrue
+console.log("Akhilesh"+false)//Akhileshfalse
+console.log("Akhilesh"+null)//Akhileshnull
+console.log("Akhilesh"+undefined);//Akhileshundefined
+
+//subtraction Opertaor
+console.log("Hello"-"Hii")//NaN
+console.log("Hello"-100)//NaN
+console.log("Hello"-true)//NaN
+console.log("Hello"-false)//NaN
+console.log(true-false)//0
+
+console.log(100-1)//99
+console.log(100-true)//99
+console.log(100-false)//100
+console.log(100-null)//100
+console.log(100-undefined)//NaN
+
+//Multiplication Opertor
+console.log("Hello"*"Hi")//NaN
+console.log("100"*3)//300
+console.log(200*false)//0
+console.log("===========================================")
+//Division
+console.log(34/2)   //17  
+console.log(34/false) //infi
+console.log(null/9)//0
+console.log(7676/undefined)//NaN
+//modulus
+console.log(6%2)       
+console.log(0%2)       
+console.log(4%0) //NaN      
+      
+
+
+
+ 
